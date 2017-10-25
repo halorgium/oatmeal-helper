66345a769b5d0ea519692fb7b59b6b69177d974d
Tim Carey-Smith <tim@spork.in>
2017-10-24 21:49:24 -0700

    Add presets for ready time

--- src/App.js
+++ src/App.js
@@ -13,20 +13,33 @@ const calculateWait = (now, ready, cook) => {
   return moment.duration(ready - now.clone().add(cook))
 }
 
-class Deltas extends React.Component {
+class Absolute extends React.Component {
   static propTypes = {
-    callback: PropTypes.func.isRequired
+    time: PropTypes.string.isRequired,
+    updater: PropTypes.func.isRequired
   }
 
   render () {
-    const { callback } = this.props
+    const { time, updater } = this.props
+
+    return <button onClick={() => updater(time)}>{time}</button>
+  }
+}
+
+class Deltas extends React.Component {
+  static propTypes = {
+    updater: PropTypes.func.isRequired
+  }
+
+  render () {
+    const { updater } = this.props
 
     return (
       <div>
-        <button onClick={() => callback(1, 'hour')}>+1h</button>
-        <button onClick={() => callback(-1, 'hour')}>-1h</button>
-        <button onClick={() => callback(15, 'minutes')}>+15m</button>
-        <button onClick={() => callback(-15, 'minutes')}>-15m</button>
+        <button onClick={() => updater(1, 'hour')}>+1h</button>
+        <button onClick={() => updater(-1, 'hour')}>-1h</button>
+        <button onClick={() => updater(15, 'minutes')}>+15m</button>
+        <button onClick={() => updater(-15, 'minutes')}>-15m</button>
       </div>
     )
   }
@@ -38,12 +51,12 @@ class Helper extends React.Component {
     ready: PropTypes.instanceOf(moment).isRequired,
     cook: PropTypes.object.isRequired,
     wait: PropTypes.object.isRequired,
-    updateReady: PropTypes.func.isRequired,
-    updateCook: PropTypes.func.isRequired
+    absolutes: PropTypes.object.isRequired,
+    deltas: PropTypes.object.isRequired
   }
 
   render () {
-    const { now, ready, cook, wait, deltas } = this.props
+    const { now, ready, cook, wait, absolutes, deltas } = this.props
 
     return (
       <div>
@@ -53,15 +66,15 @@ class Helper extends React.Component {
         </div>
         <div>
           <p>Time to be ready</p>
-          <button onClick={() => '6:30'} />
-          <button onClick={() => '7:00'} />
-          <button onClick={() => '8:00'} />
-          <Deltas callback={deltas.ready} />
+          <Absolute time='6:30' updater={absolutes.ready} />
+          <Absolute time='7:00' updater={absolutes.ready} />
+          <Absolute time='8:00' updater={absolutes.ready} />
+          <Deltas updater={deltas.ready} />
           <div>{ready.format('HH:mm')}</div>
         </div>
         <div>
           <p>Time to cook</p>
-          <Deltas callback={deltas.cook} />
+          <Deltas updater={deltas.cook} />
           <div>{cook.format('HH:mm')}</div>
         </div>
         <div>
@@ -73,8 +86,8 @@ class Helper extends React.Component {
   }
 }
 
-const makeReady = now => {
-  const ready = moment('0630', 'HHmm')
+const makeAbsolute = (time, now) => {
+  const ready = moment(time, 'HH:mm')
 
   if (ready.isAfter(now)) {
     return ready
@@ -89,13 +102,17 @@ class App extends React.Component {
 
     const now = moment()
     const cook = moment.duration('02:30')
-    const ready = makeReady(now)
+    const ready = makeAbsolute('06:30', now)
 
     this.state = { now, cook, ready }
 
     this.timer = moment.duration(10, 'seconds').timer({ start: true, loop: true }, () => {
       this.setState({now: moment()})
     })
+
+    this.absolutes = {
+      ready: this.absolute('ready').bind(this)
+    }
 
     this.deltas = {
       ready: this.delta('ready').bind(this),
@@ -114,10 +131,18 @@ class App extends React.Component {
         </header>
         <Helper
           now={now} ready={ready} cook={cook} wait={wait}
-          deltas={this.deltas}
+          absolutes={this.absolutes} deltas={this.deltas}
         />
       </div>
     )
+  }
+
+  absolute (key) {
+    return (time) => {
+      const { now } = this.state
+      const value = makeAbsolute(time, now)
+      this.setState({ [key]: value })
+    }
   }
 
   delta (key) {
